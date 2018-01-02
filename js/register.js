ceb09fcee6fb4824e0d43ab3a0b225bec5fc00a7
Claudiagari <claudia.garibotto@gmail.com>
2018-01-02 09:13:34 -0500

    comentando js register

--- js/register.js
+++ js/register.js
@@ -1,14 +1,17 @@
+// Cargando documento
 $(document).ready(function() {
+  // Llamando las variables 
   var $firstname = $('#firstname');
   var $lastname = $('#lastname');
   var $email = $('#email');
   var $checkbox = $(':checkbox');
   var $submit = $('.submit');
+  // Variables inicializadas en false para validar
   validateFirst = false;
   validateLast = false;
   validateEmail = false;
   validateCheckbox = false;
-
+  // Obteniendo el contenido validando que sea letras maximo de 10
   $firstname.on('input', function() {
     var PATTERNNAME = /^([A-z ñáéíóú]{2,10})$/;
     if (PATTERNNAME.test($(this).val())) {
@@ -18,6 +21,7 @@ $(document).ready(function() {
       disabledButton();
     }
   });
+  // Obteniendo el contenido validando que sea letras maximo de 15
   $lastname.on('input', function() {
     var PATTERNLASTNAME = /^([A-z ñáéíóú]{2,15})$/;
     if (PATTERNLASTNAME.test($(this).val())) {
@@ -27,6 +31,7 @@ $(document).ready(function() {
       disabledButton();
     }
   });
+  // Validando el email que contenga @ y .
   $email.on('input', function() {
     var PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
     if (PATTERNEMAIL.test($(this).val())) {
@@ -36,6 +41,7 @@ $(document).ready(function() {
       disabledButton();;
     }
   });
+  // Validando que este maracado el checkbox
   $checkbox.on('click', function(event) {
     if ($checkbox.prop('checked')) {
       validateCheckbox = true;
@@ -44,11 +50,13 @@ $(document).ready(function() {
       disabledButton();
     }
   });
+  // Funcion que comprueba que los cuatro valores se encuntren en true para activar en boton
   function activeButton() {
-    if (validateFirst && validateLast &&  validateEmail && validateCheckbox) {
+    if (validateFirst && validateLast && validateEmail && validateCheckbox) {
       $submit.removeAttr('disabled');
     }
   }
+  // Funcion que deshabilita el boton
   function disabledButton() {
     $submit.attr('disabled', 'disabled');
   }
